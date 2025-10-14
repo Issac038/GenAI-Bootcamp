a599bb5ab8b53174ed36a2dc1103dc9af0aefddd
Riisshi <riiisshhii@gmail.com>
2025-10-14 17:10:46 +0530

    basic server done

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const PORT = 3000
+const app = express()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connection successful"))
+    .catch(() => console.log("MongoDB connection failed"))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
